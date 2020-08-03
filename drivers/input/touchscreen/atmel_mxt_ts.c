408af8e6e4fb8de993dbaabcd2b1ac10dc5bed33
Tom Rix <trix@redhat.com>
2020-08-03 06:44:01 -0700

    input: atmel_mxt_ts: fix double free
    Clang static analysis reports this error
    
    atmel_mxt_ts.c:1850:2: warning: Attempt to free released memory
            kfree(id_buf);
            ^~~~~~~~~~~~~
    
    The problem is with this code block
    
    data->raw_info_block = id_buf;
    ...
    error = mxt_parse_object_table(data, id_buf + MXT_OBJECT_START);
    if (error) {
    	dev_err(&client->dev, "Error %d parsing object table\n", error);
    	mxt_free_object_table(data);
    	goto err_free_mem;
    }
    
    mxt_free_object_table() frees id_buf
    
    kfree(data->raw_info_block);
    
    So skip over the second free
    
    Fixes: 068bdb67ef74 ("Input: atmel_mxt_ts - fix the firmware update")
    
    Signed-off-by: Tom Rix <trix@redhat.com>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -1841,7 +1841,7 @@ static int mxt_read_info_block(struct mxt_data *data)
 	if (error) {
 		dev_err(&client->dev, "Error %d parsing object table\n", error);
 		mxt_free_object_table(data);
-		goto err_free_mem;
+		goto err_free_mem1;
 	}
 
 	data->object_table = (struct mxt_object *)(id_buf + MXT_OBJECT_START);
@@ -1850,6 +1850,7 @@ static int mxt_read_info_block(struct mxt_data *data)
 
 err_free_mem:
 	kfree(id_buf);
+err_free_mem1:
 	return error;
 }
 
