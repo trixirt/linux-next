d4579318ee4294e3f20cd0350d55c4034a1f1943
Tom Rix <trix@redhat.com>
2020-11-26 09:38:52 -0800

    x86/hyper-v: remove trailing semicolon in macro definition
    The definition of a function-like macro should not have a trailing semicolon.
    The macro use will already have a semicolon.
    
    Signed-off-by: Tom Rix <trix@redhat.com>

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -52,7 +52,7 @@ typedef int (*hyperv_fill_flush_list_func)(
 #define hv_set_clocksource_vdso(val) \
 	((val).vdso_clock_mode = VDSO_CLOCKMODE_HVCLOCK)
 #define hv_enable_vdso_clocksource() \
-	vclocks_set_used(VDSO_CLOCKMODE_HVCLOCK);
+	vclocks_set_used(VDSO_CLOCKMODE_HVCLOCK)
 #define hv_get_raw_timer() rdtsc_ordered()
 #define hv_get_vector() HYPERVISOR_CALLBACK_VECTOR
 
