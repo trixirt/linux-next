08aa241604e2f639e00aa97c88eacf1a9315592a
Tom Rix <trix@redhat.com>
2023-05-01 07:28:29 -0400

    btrfs: replace else-statement with initialization
    A small optimization
    Move the default value of transid to its initialization
    and remove the else-statement.
    
    Signed-off-by: Tom Rix <trix@redhat.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -3132,14 +3132,13 @@ out:
 static noinline long btrfs_ioctl_wait_sync(struct btrfs_fs_info *fs_info,
 					   void __user *argp)
 {
-	u64 transid;
+	u64 transid = 0;  /* current trans */
 
 	if (argp) {
 		if (copy_from_user(&transid, argp, sizeof(transid)))
 			return -EFAULT;
-	} else {
-		transid = 0;  /* current trans */
 	}
+
 	return btrfs_wait_for_commit(fs_info, transid);
 }
 
